c385cdc042c936e987dee0e97f321ec2b76f819e
Jamile Moreira <jamilemoreira2310@gmail.com>
2023-07-28 23:02:20 -0300

    restando numeros e sinal =

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -5,6 +5,9 @@ import { Box } from "@mui/material";
 
 export default function Calculator() {
     const [number, setNumber] = useState(0);
+    const [oldNumber, setOldNumber] = useState(0);
+    const [acion, setAcion] = useState();
+
 
     function inputNumber(e){
         let input= e.target.value;
@@ -29,9 +32,30 @@ export default function Calculator() {
     function operator(){
         if(number>0){
             setNumber(-number)
+        }else{
+            setNumber(Math.abs(number));
         }
     }
 
+    function selectAcion(e){
+        let operatorSelect = e.target.value;
+        setAcion(operatorSelect)
+        setOldNumber(number)
+        setNumber(0);
+    }
+
+    function calculate() {
+        if (acion === "/"){
+            setNumber(oldNumber / number);
+        } else if (acion === 'X') {
+            setNumber(parseFloat(oldNumber) * parseFloat(number));
+        } else if (acion === '-') {
+            setNumber(parseFloat(oldNumber) - parseFloat(number));
+        } else if (acion === '+') {
+            setNumber(parseFloat(oldNumber) + parseFloat(number));
+        }
+    }
+        
     return (
         <div className="divContainer">
         <Box m={5}/>
@@ -42,25 +66,25 @@ export default function Calculator() {
             <Box m={12} />
             <h1 className="result">{number}</h1>
             <button onClick={clear}>AC</button>
-            <button onAuxClick={operator}>+-</button>
+            <button onClick={operator}>+-</button>
             <button onClick={porcentage}>%</button>
-            <button className="side">/</button>
+            <button className="side" onClick={selectAcion} value='/'>/</button>
             <button className="sideLeft" onClick={inputNumber} value={7}>7</button>
             <button className="sideLeft" onClick={inputNumber} value={8}>8</button>
             <button className="sideLeft" onClick={inputNumber} value={9}>9</button>
-            <button className="side">X</button>
+            <button className="side" onClick={selectAcion} value='X'>X</button>
             <button className="sideLeft" onClick={inputNumber} value={4}>4</button>
             <button className="sideLeft" onClick={inputNumber} value={5}>5</button>
             <button className="sideLeft" onClick={inputNumber} value={6}>6</button>
-            <button className="side">-</button>
+            <button className="side" onClick={selectAcion} value='-'>-</button>
             <button className="sideLeft" onClick={inputNumber} value={1}>1</button>
             <button className="sideLeft" onClick={inputNumber} value={2}>2</button>
             <button className="sideLeft" onClick={inputNumber} value={3}>3</button>
-            <button className="side">+</button>
+            <button className="side" onClick={selectAcion} value='+'>+</button>
             <button className="sideLeft" onClick={inputNumber} value={0}>0</button>
-            <button className="sideLeft">,</button>
+            <button className="sideLeft" onClick={inputNumber} value={'.'}>,</button>
             <button className="sideLeft" style={{ visibility:"hidden" }}>,</button>
-            <button className="side">=</button>
+            <button className="side" onClick={calculate}>=</button>
           
          </div>
          </div>
